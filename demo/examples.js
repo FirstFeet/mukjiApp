cd50ad291174fa2500366b63aa9121fd9112041c
narayanaraj <narayanarajk23@gmail.com>
2016-12-24 11:19:19 +0530

    updates

--- demo/examples.js
+++ demo/examples.js
@@ -5,55 +5,55 @@ $( document ).on( "pageinit", "#demoPage", function( event ) {
 
     $("#default").on("tap", function() {
         $.mobile.toast({
-            message: "Live long and Mukundan!"
+            message: "Team mananger Mukundan!"
         });
     });
 
     $("#longer").on("tap", function() {
         $.mobile.toast({
-            message: "Live long and Mukundan!",
+            message: "Team mananger Mukundan!",
             duration: "long"
         });
     });
 
     $("#top").on("tap", function() {
         $.mobile.toast({
-            message: "Live long and Mukundan!",
+            message: "Team mananger Mukundan!",
             position: "top"
         });
     });
 
     $("#center").on("tap", function() {
         $.mobile.toast({
-            message: "Live long and Mukundan!",
+            message: "Team mananger Mukundan!",
             position: "center"
         });
     });
 
     $("#bottom").on("tap", function() {
         $.mobile.toast({
-            message: "Live long and Mukundan!",
+            message: "Team mananger Mukundan!",
             position: "bottom"
         });
     });
 
     $("#custom").on("tap", function() {
         $.mobile.toast({
-            message: "Live long and Mukundan!",
+            message: "Team mananger Mukundan!",
             classOnOpen: "pomegranate"
         });
     });
 
     $("#custom-kitkat").on("tap", function() {
         $.mobile.toast({
-            message: "Live long and Mukundan!",
+            message: "Team mananger Mukundan!",
             classOnOpen: "kitkat"
         });
     });
 
     $("#close-event").on("tap", function() {
         $.mobile.toast({
-            message: "Live long and Mukundan!",
+            message: "Team mananger Mukundan!",
             afterclose: function(event, ui) {
                 alert("Toast closed!");
             }
@@ -62,14 +62,14 @@ $( document ).on( "pageinit", "#demoPage", function( event ) {
 
     $("#animate-css").on("tap", function() {
         $.mobile.toast({
-            message: "Live long and Mukundan!",
+            message: "Team mananger Mukundan!",
             classOnOpen: "animated bounceInUp"
         });
     });
 
     $("#animate-css2").on("tap", function() {
         $.mobile.toast({
-            message: "Live long and Mukundan!",
+            message: "Team mananger Mukundan!",
             classOnOpen: "animated slideInLeft",
             classOnClose: "slideOutRight"
         });
